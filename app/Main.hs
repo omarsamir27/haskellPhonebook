47d184f68095afdc14f834a68133f1a4ab613280
Omar Mohamed Samir <39497615+omarsamir27@users.noreply.github.com>
2020-08-15 01:36:49 +0200

    Added sorting functionality

--- app/Main.hs
+++ app/Main.hs
@@ -5,27 +5,32 @@ import qualified System.IO.Strict as SIO
 import Data.List
 import Data.List.Split
 import Data.List.Utils
+import Data.Time.Calendar
 
-data Date = Date {day :: Int ,
-                  month :: Int ,
-                  year :: Int} deriving (Read,Eq)
+--data Date = Date {day :: Int ,
+--                  month :: Int ,
+--                  year :: Int} deriving (Read,Eq)
+--
+--instance Show Date where
+--    show date = show (day date) ++ "-" ++ show (month date) ++ "-" ++ show (year date)
 
-instance Show Date where
-    show date = show (day date) ++ "-" ++ show (month date) ++ "-" ++ show (year date)
+createDate :: String -> Day
+createDate date = let [day,month,year] = wordsBy (=='-') date in fromGregorian (read year)  (read month)  (read day)
 
-createDate :: String -> Date
-createDate date = let [x,y,z] = wordsBy (=='-') date in Date (read x)  (read y)  (read z)
+reverseDayFormat :: Day -> String
+reverseDayFormat date = (show day) ++ "-" ++ (show month) ++ "-" ++  (show year) 
+                        where (year , month , day) = toGregorian date
 
 
 data Contact = Contact { lastname :: String,
                          firstname :: String,
-                         dob :: Date,
+                         dob :: Day,
                          address :: String,
                          email :: String ,
                          phonenumber :: String
                          } deriving (Eq)
 instance Show Contact  where
-    show contact = firstname contact ++ "\t" ++ lastname contact ++ "\t" ++ show (dob contact) ++ "\t" ++ address contact ++ "\t" ++ email contact ++ "\t" ++ phonenumber contact
+    show contact = firstname contact ++ "\t" ++ lastname contact ++ "\t" ++  (reverseDayFormat (dob contact)) ++ "\t" ++ address contact ++ "\t" ++ email contact ++ "\t" ++ phonenumber contact
 
    
    
@@ -48,6 +53,21 @@ searchContact phoneDB name = [contact | contact <- phoneDB , (lastname contact =
 
 contactPrinter :: [Contact] -> IO ()
 contactPrinter =  mapM_ print
+
+ageSort :: [Contact] -> [Contact]
+ageSort = sortBy (\a b -> (dob a) `compare` (dob b) )
+
+nameSort :: [Contact] -> [Contact]
+nameSort = sortBy (\a b -> (firstname a) `compare` (firstname b) )
+        
+contactSort :: [Contact] -> String -> [Contact]
+contactSort db "1" =  reverse $ ageSort db
+contactSort db "2" = ageSort db
+contactSort db "3" = nameSort db
+contactSort db "4" = reverse $ nameSort db
+contactSort db _ = db 
+       
+             
 
 
 
@@ -76,6 +96,10 @@ dispatch db "4" filepath = do
                         putStrLn "Name to Delete: "
                         nametoDel <- getLine
                         addContact (db \\ searchContact db nametoDel) filepath WriteMode
+dispatch db "5" _ = do
+                    putStrLn "1. Age Ascendingly\n2. Age Descendingly\n 3. FName Ascendingly\n 4.FName Descendingly"
+                    priority <- getLine
+                    contactPrinter $ contactSort db priority
 dispatch db _ filepath = do
                  putStrLn "Wrong Choice"
                  homeScreen  filepath
@@ -85,7 +109,7 @@ homeScreen  filepath = do
             phoneBook <- openFile filepath ReadMode
             addressbook <- SIO.hGetContents phoneBook
             putStrLn "What to do"
-            putStrLn "1. Print Book \n2. Search by name \n3. Add Contact \n4. Delete Contact"
+            putStrLn "1. Print Book \n2. Search by name \n3. Add Contact \n4. Delete Contact \n5.Sort and Print"
             choice <- getLine
             let phoneDB = loadDB addressbook
             dispatch phoneDB choice filepath
